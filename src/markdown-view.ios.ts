cc145df586217597f50dfb3204f6a7e8ac2c9d6f
Fernando Gonzalez Goncharov <ditoglez@gmail.com>
2019-11-08 00:47:18 +0200

    feat(ios): make links work in iOS. (#5)
    * feat(ios): replace UILabel with UITextView
    
    * chore: updates to latest nativescript
    
    * fix(travis-ci): Bumps node version to 10
    
    * chore: Updates to xcode10.2

--- src/markdown-view.ios.ts
+++ src/markdown-view.ios.ts
@@ -2,19 +2,22 @@ import { markdownProperty, MarkdownViewBase } from './markdown-view.common';
 
 export class MarkdownView extends MarkdownViewBase {
 
-    _ios: UILabel;
+    _ios: UITextView;
 
     constructor() {
         super();
     }
 
     public createNativeView() {
-        this._ios = super.createNativeView() as UILabel;
+        this._ios = super.createNativeView() as UITextView;
         return this._ios;
     }
 
     [markdownProperty.setNative](markdown: string) {
         const md = TSMarkdownParser.standardParser();
+        this._ios.editable = false;
+        this._ios.selectable = true;
+
         this._ios.attributedText = md.attributedStringFromMarkdown(markdown);
     }
 
