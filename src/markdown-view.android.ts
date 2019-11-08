ebd83fa73a883ea43f30b7e939f3dae93e0cb59d
Fernando Gonzalez Goncharov <fernando@rocketloop.de>
2019-11-08 18:41:00 +0200

    feat: make font size configurable on android

--- src/markdown-view.android.ts
+++ src/markdown-view.android.ts
@@ -1,29 +1,36 @@
-import { markdownProperty, MarkdownViewBase } from './markdown-view.common';
-
+import { fontSizeProperty, markdownProperty, MarkdownViewBase } from './markdown-view.common';
+​
 declare var ru: any;
-
+​
 export class MarkdownView extends MarkdownViewBase {
-
+​
     _android: any = null; // android.widget.TextView
-
+​
     markwon: any;
-
+​
+    private _fontSize = 14;
+​
     constructor() {
         super();
     }
-
+​
     get android(): android.widget.TextView {
         return this._android;
     }
-
+​
     public createNativeView() {
         this.markwon = ru.noties.markwon.Markwon.create(this._context);
         this._android = new android.widget.TextView(this._context);
         return this._android;
     }
-
+​
+    [fontSizeProperty.setNative](size: number) {
+        this._fontSize = size;
+    }
+​
     [markdownProperty.setNative](markdown: string) {
+        this._android.setTextSize(Number(this._fontSize));
         this.markwon.setMarkdown(this.nativeView, markdown);
     }
-
+​
 }
