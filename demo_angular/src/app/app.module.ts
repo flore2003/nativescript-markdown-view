ef5bd3c87fa78da9d9d2db1f5e8d6e3014ebf17a
Florian Otto <florian.otto@ecsec.de>
2020-05-26 13:39:47 +0200

    adjust import

--- demo_angular/src/app/app.module.ts
+++ demo_angular/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MarkdownComponent } from "./markdown/markdown.component";
 
-import { MarkdownViewModule } from "../../../src/angular";
+import { MarkdownViewModule } from "nativescript-markdown-view/angular";
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from "nativescript-angular/forms";
